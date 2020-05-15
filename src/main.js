62fd71339d490784a4ccae2132d545a7c330e8e5
Jenky1031 <869386961@qq.com>
2020-05-16 01:01:23 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+/* 300ms点击延迟 */
+import fastClick from "fastclick";
+import "./assets/styles/reset.css";
+/* 一像素边框 */
+import "./assets/styles/border.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+fastClick.attach(document.body);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	render: (h) => h(App),
+}).$mount("#app");
